09c482123f238f926ef2f0fcf37b9243c466db1d
Nikhil Kumar Yadav <142158924+NikhilKY64@users.noreply.github.com>
2026-04-11 08:20:32 +0530

    Attendance Management-CLI

---
+++ CRMS.py
@@ -0,0 +1,84 @@
+import mysql.connector
+from tabulate import tabulate
+
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin'
+)
+
+cursor = conn.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS CRMS")
+conn.database = 'CRMS'
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS students(
+        id int AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50),
+        age INT,
+        attendence ENUM('present', 'absent') NOT NULL
+)""")
+
+while True:
+
+    print("Options")
+    print("1) Insert_data")
+    print("2) Update_data")
+    print("3) Delete_data")
+    print("4) Show_table")
+    print("5) Exit")
+
+
+    option = int(input("Enter your option: "))
+
+
+    if option == 1:
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+        attendence = input("Enter present or absent: ")
+        cursor.execute("INSERT INTO students (name, age, attendence) VALUES (%s,%s,%s)",(name, age, attendence))
+        conn.commit()
+        print('Data inserted successfully.')
+
+    elif option == 2:
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+        attendence = input("Enter present or absent: ")
+        cursor.execute("UPDATE students SET age=%s, attendence=%s WHERE name=%s", (age, attendence, name))
+        conn.commit()
+        print("Data updated successfully.")
+
+    elif option == 3:
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+        attendence = input("Enter present or absent: ")
+        cursor.execute("DELETE FROM students WHERE name=%s", (name,))
+        conn.commit()
+        print("Data deleted successfully.")
+
+    elif option == 4:
+
+        name = input("Enter your name: ")
+        cursor.execute("SELECT * FROM students WHERE name=%s", (name,))
+        data = cursor.fetchall()
+        if data:
+            print("Data found!")
+            print(tabulate(data, headers=["id", "name", "age", "attendence"], tablefmt="rounded_grid"))
+            # table format options: "plain", "grid", "heavy_grid", "double_grid", "rounded_grid", etc.
+
+        else:
+            print("Data not found!")
+
+    elif option == 5:
+        print("Exiting the program...")
+        break
+
+    else:
+        print("Invalid option. Please try again.")
+    
+    print("Do you want to continue? (yes/no)")
+
+    continue_option = input().lower()
+    if continue_option != "yes" and continue_option != "y":
+        print("Exiting the program...")
+        break   
